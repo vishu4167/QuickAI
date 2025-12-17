97971273ef4236386d493ee1366eddd9fb92ec78
Vishu marwah <marwahvishu2008@gmail.com>
2025-12-17 23:06:25 +0530

    Remove dist folder

--- server/dist/configs/db.js
+++
@@ -1,4 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-const databaseUrl = process.env.DATABASE_URL;
-const sql = neon(databaseUrl);
-export default sql;
--- server/dist/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { requireAuth } from "@clerk/express";
-import { getUserCreations, getPublishedCreations, toggleLikeCreations, } from "../controllers/usercontroller.js";
-const userRouter = express.Router();
-userRouter.get("/get-user-creations", requireAuth(), getUserCreations);
-userRouter.get("/get-published-creations", getPublishedCreations);
-userRouter.post("/toggle-like-creations", requireAuth(), toggleLikeCreations);
-export default userRouter;
